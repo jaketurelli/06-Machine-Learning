4f1e8f01ccdded7a8c1915bbe7c037af5f74172f
jaketurelli <jake.turelli@gmail.com>
2018-01-28 17:23:54 -0800

    adding perceptron

--- hw 2.py
+++ hw 2.py
@@ -7,35 +7,6 @@ Created on Mon Jan 22 21:34:13 2018
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import *
-
-def getValues(n):
-    xb = (np.random.rand(n,1)*2-1)/2-0.5
-    yb = (np.random.rand(n,1)*2-1)/2+0.5
-    tb = np.ones([n,1])
-    cb = np.tile('k',(n,1))
-    xr = (np.random.rand(n,1)*2-1)/2+0.5
-    yr = (np.random.rand(n,1)*2-1)/2-0.5
-    tr = -np.ones([n,1])
-    cr = np.tile('r',(n,1))
-    b = np.concatenate((xb,yb,tb,cb),axis=1)
-    r = np.concatenate((xr,yr,tr,cr),axis=1)
-    inputs = np.concatenate((b,r),axis=0)
-    
-    myDF = DataFrame({
-    'x1' : inputs[:,0],
-    'x2' : inputs[:,1],
-    'Targets' : inputs[:,2],
-    'Color': inputs[:,3]
-    })
-
-    return(myDF)
-
-nValues = 500
-myValues = getValues(nValues)
-#print(myValues.Color)
-
-#plt.scatter(myValues.x1, myValues.x2, c=colormap[myValues.Targets], s=40)
-plt.scatter(myValues.x1, myValues.x2, c=myValues.Color, s=40)
 """
 Help:
 http://stamfordresearch.com/scikit-learn-perceptron/
@@ -56,6 +27,34 @@ Plot the examples {(xn,yn)} as well as the target function f on a plane.
 Be sure to mark the examples from different classes differently,
 and add labels to the axes of the plot.
 """
+def getValues(n):
+    xb = (np.random.rand(n,1)*2-1)/2-0.5
+    yb = (np.random.rand(n,1)*2-1)/2+0.5
+    tb = np.ones([n,1])
+    cb = np.tile('k',(n,1))
+    xr = (np.random.rand(n,1)*2-1)/2+0.5
+    yr = (np.random.rand(n,1)*2-1)/2-0.5
+    tr = -np.ones([n,1])
+    cr = np.tile('r',(n,1))
+    b = np.concatenate((xb,yb,tb,cb),axis=1)
+    r = np.concatenate((xr,yr,tr,cr),axis=1)
+    inputs = np.concatenate((b,r),axis=0)
+    myDF = DataFrame({
+    'x1' : inputs[:,0],
+    'x2' : inputs[:,1],
+    'Targets' : inputs[:,2],
+    'Color': inputs[:,3]
+    })
+
+    return(myDF)
+
+nValues = 10
+myValues = getValues(nValues)
+fig = plt.figure()
+#x = np.arange(min(myValues.x1), max(myValues.x1)+1, 1.0)
+#np.arange()
+plt.scatter(myValues.x1, myValues.x2, c=myValues.Color, s=40)
+plt.show()
 
 """
 (b) Run the perceptron learning algorithm on the data set above.
@@ -64,6 +63,38 @@ Plot the examples {(xn,yn)}, the target function f, and the final hypothesis
 g in the same figure.
 Comment on whether f is close to g.
 """
+
+def response(x,w):
+    """ perceptron output """
+    y = x[0]*w[0]+x[1]*w[1] # dot product between w and x
+    if y >= 0:
+        return 1
+    else:
+        return -1
+    
+    
+w = np.random.rand(nValues,1)*2-1 # weights
+learned = False
+epochs = 0
+while not learned:
+    globalError = 0.0
+    for x in myValues: # for each sample
+        r = response(x,w)    
+        if x[2] != r: # if we have a wrong response
+            iterError = x[2] - r # desired response - actual response
+            self.updateWeights(x,iterError)
+            globalError += abs(iterError)
+            iteration += 1
+        if globalError == 0.0 or iteration >= 100: # stop criteria
+            print('iterations',iteration)
+            learned = True # stop learning
+    
+    
+    
+    
+
+
+
 
 """
 (c) Repeat everything in (b) with another randomly generated data set of size 20.
---
+++ perceptronPractice.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 16:31:24 2018
+Perception Practice
+@author: Jake
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import *
+#from pylab import rand,plot,show,norm
+
+class Perceptron:
+ def __init__(self):
+  """ perceptron initialization """
+  self.w = np.random.rand(2)*2-1 # weights
+  self.learningRate = 0.1
+
+ def response(self,x):
+  """ perceptron output """
+  y = x[0]*self.w[0]+x[1]*self.w[1] # dot product between w and x
+  if y >= 0:
+   return 1
+  else:
+   return -1
+
+ def updateWeights(self,x,iterError):
+  """
+   updates the weights status, w at time t+1 is
+       w(t+1) = w(t) + learningRate*(d-r)*x
+   where d is desired output and r the perceptron response
+   iterError is (d-r)
+  """
+  self.w[0] += self.learningRate*iterError*x[0]
+  self.w[1] += self.learningRate*iterError*x[1]
+
+ def train(self,data):
+  """ 
+   trains all the vector in data.
+   Every vector in data must have three elements,
+   the third element (x[2]) must be the label (desired output)
+  """
+  learned = False
+  iteration = 0
+  while not learned:
+   globalError = 0.0
+   for x in data: # for each sample
+    r = self.response(x)    
+    if x[2] != r: # if we have a wrong response
+     iterError = x[2] - r # desired response - actual response
+     self.updateWeights(x,iterError)
+     globalError += abs(iterError)
+   iteration += 1
+   if globalError == 0.0 or iteration >= 100: # stop criteria
+    print('iterations',iteration)
+    learned = True # stop learning
+    
+    
+def generateData(n):
+    """ 
+      generates a 2D linearly separable dataset with n samples. 
+      The third element of the sample is the label
+    """
+    xb = np.arange(n)
+    delta = np.random.uniform(-10,10, size=(n,))
+    xb = (np.random.rand(n,1)*2-1)/2-0.5
+    yb = (np.random.rand(n,1)*2-1)/2+np.random.rand(1,1)
+    tb = np.ones([n,1])
+    xr = (np.random.rand(n,1)*2-1)/2+0.5
+    yr = (np.random.rand(n,1)*2-1)/2-np.random.rand(1,1)
+    tr = -np.ones([n,1])
+    b = np.concatenate((xb,yb,tb),axis=1)
+    r = np.concatenate((xr,yr,tr),axis=1)
+    inputs = np.concatenate((b,r),axis=0)
+    return inputs
+
+trainset = generateData(100) # train set generation
+perceptron = Perceptron()   # perceptron instance
+perceptron.train(trainset)  # training
+testset = generateData(1000)  # test set generation
+
+# Perceptron test
+for x in testset:
+ r = perceptron.response(x)
+ if r != x[2]: # if the response is not correct
+  print('error')
+ if r == 1:
+  plt.plot(x[0],x[1],'ob')  
+ else:
+  plt.plot(x[0],x[1],'or')
+
+# plot of the separation line.
+# The separation line is orthogonal to w
+n = np.linalg.norm(perceptron.w)
+ww = perceptron.w/n
+ww1 = [ww[1],-ww[0]]
+ww2 = [-ww[1],ww[0]]
+plt.plot([ww1[0], ww2[0]],[ww1[1], ww2[1]],'--k')
+plt.show()
